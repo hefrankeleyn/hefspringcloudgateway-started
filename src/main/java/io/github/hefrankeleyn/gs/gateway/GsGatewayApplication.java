5634ad5af18fb69aa7a4b09c5c0989716dc632f0
Li Fei <hefrankeleyn@gamil.com>
2024-08-27 23:44:28 +0800

    Step 1.3： 添加单元测试

--- src/main/java/io/github/hefrankeleyn/gs/gateway/GsGatewayApplication.java
+++ src/main/java/io/github/hefrankeleyn/gs/gateway/GsGatewayApplication.java
@@ -2,6 +2,8 @@ package io.github.hefrankeleyn.gs.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@EnableConfigurationProperties(UriConfiguration.class)
 @RestController
 public class GsGatewayApplication {
 
@@ -18,15 +21,16 @@ public class GsGatewayApplication {
     }
 
     @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+        String httpbin = uriConfiguration.getHttpbin();
         return builder.routes()
                 .route(p->p.path("/get")
                         .filters(f->f.addRequestHeader("Hello", "World"))
-                        .uri("http://httpbin.org:80"))
+                        .uri(httpbin))
                 .route(p->p.host("*.circuitbreaker.com")
                         .filters(f->f.circuitBreaker(config->config.setName("mycmd")
                                 .setFallbackUri("forward:/fallback")))
-                        .uri("http://httpbin.org:80"))
+                        .uri(httpbin))
                 .build();
     }
 
@@ -35,4 +39,17 @@ public class GsGatewayApplication {
         return Mono.just("fallback");
     }
 
+}
+
+@ConfigurationProperties
+class UriConfiguration {
+    private String httpbin = "http://httpbin.org:80";
+
+    public String getHttpbin() {
+        return httpbin;
+    }
+
+    public void setHttpbin(String httpbin) {
+        this.httpbin = httpbin;
+    }
 }
--- src/test/java/io/github/hefrankeleyn/gs/gateway/GsGatewayApplicationTests.java
+++ src/test/java/io/github/hefrankeleyn/gs/gateway/GsGatewayApplicationTests.java
@@ -1,13 +1,32 @@
 package io.github.hefrankeleyn.gs.gateway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-class GsGatewayApplicationTests {
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.springframework.cloud.contract.verifier.assertion.SpringCloudContractAssertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {"httpbin=http://localhost:${wiremock.server.port}"})
+@AutoConfigureWireMock(port = 0)
+public class GsGatewayApplicationTests {
+
+    @Autowired
+    private WebTestClient webTestClient;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        stubFor(get(urlEqualTo("/get")).willReturn(aResponse().withBody("{\"headers\":{\"Hello\":\"World\"}}")
+                .withHeader("Content-Type", "application/json")));
+        stubFor(get(urlEqualTo("/delay/3")).willReturn(aResponse().withBody("no fallback").withFixedDelay(3000)));
+
+        webTestClient.get().uri("/get").exchange().expectStatus().isOk().expectBody().jsonPath("$.headers.Hello").isEqualTo("World");
+        webTestClient.get().uri("/delay/3").header("Host", "www.circuitbreaker.com")
+                .exchange().expectStatus().isOk().expectBody()
+                .consumeWith(response->assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes()));
     }
 
 }
