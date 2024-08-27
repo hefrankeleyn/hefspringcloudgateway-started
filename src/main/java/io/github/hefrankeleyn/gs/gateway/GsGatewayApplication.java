9aae4920708413810171bbcc260165b710444eba
Li Fei <hefrankeleyn@gamil.com>
2024-08-27 19:46:52 +0800

    Step 1.2: 添加断路器，请求失败，请求到fallback

--- src/main/java/io/github/hefrankeleyn/gs/gateway/GsGatewayApplication.java
+++ src/main/java/io/github/hefrankeleyn/gs/gateway/GsGatewayApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
 public class GsGatewayApplication {
 
     public static void main(String[] args) {
@@ -20,9 +24,15 @@ public class GsGatewayApplication {
                         .filters(f->f.addRequestHeader("Hello", "World"))
                         .uri("http://httpbin.org:80"))
                 .route(p->p.host("*.circuitbreaker.com")
-                        .filters(f->f.circuitBreaker(config->config.setName("mycmd")))
+                        .filters(f->f.circuitBreaker(config->config.setName("mycmd")
+                                .setFallbackUri("forward:/fallback")))
                         .uri("http://httpbin.org:80"))
                 .build();
+    }
+
+    @RequestMapping(value = "/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
     }
 
 }
