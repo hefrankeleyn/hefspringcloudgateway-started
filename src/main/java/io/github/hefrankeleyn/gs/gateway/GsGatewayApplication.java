3f565a94344f65621164bbee1353a1be1846cfbb
Li Fei <hefrankeleyn@gamil.com>
2024-08-27 19:42:09 +0800

    Step 1.1 测试get 和断路器
    1. get 测试路由；
    2. 断路器，测试超时；

---
+++ src/main/java/io/github/hefrankeleyn/gs/gateway/GsGatewayApplication.java
@@ -0,0 +1,28 @@
+package io.github.hefrankeleyn.gs.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p->p.path("/get")
+                        .filters(f->f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .route(p->p.host("*.circuitbreaker.com")
+                        .filters(f->f.circuitBreaker(config->config.setName("mycmd")))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+}
---
+++ src/test/java/io/github/hefrankeleyn/gs/gateway/GsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hefrankeleyn.gs.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
